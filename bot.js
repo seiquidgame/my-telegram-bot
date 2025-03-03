51c3a0881d652394504b5bf225bb397133c4bb77
Harith Bjork <seiquidgame@gmail.com>
2025-03-03 13:15:30 +0800

    added q

--- bot.js
+++ bot.js
@@ -16,7 +16,8 @@ bot.on('message', (msg) => {
         { keywords: ["telegram"], response: "You are in our official TG: https://t.me/+LcIL0smlWs02NTE9" },
         { keywords: ["discord"], response: "Here: https://discord.gg/a8Eq4tUbVY" },
         { keywords: ["referral"], response: "On airdrop.seiquid.xyz, there is Check Submission button, enter your Wallet Address to see how many referrals you have, and other infos." },
-        { keywords: ["bot", "robot", "machine"], response: "The only bot here is your mom." }
+        { keywords: ["bot", "robot", "machine"], response: "The only bot here is your mom." },
+        { keywords: ["win", "join"], response: "You can join and win thru airdrop.seiquid.xyz, you will get your referral code once you submit the form. Use your referral code to invite more people in. You can check the number of referrals thru the Check Submission page located on the same website I gave you. Don't abuse in submitting form using your referral code as our system will auto-detect abuse, plus we manually check it as well for 2nd verification. Top referrals will be ranked and prizes are on the list of the same page I gave you." }
     ];
 
     // Check if the user's message contains any keyword
